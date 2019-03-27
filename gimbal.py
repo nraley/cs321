fc031d7fc7a0214a19aa4b634fff97142f36f57e
Nelson <nelson@raleygroup.com>
2019-03-27 18:32:34 -0400

    added gimbal.py

--- gimbal.py
+++ gimbal.py
@@ -1,23 +1,3 @@
-#!/usr/bin/python
-#--------------------------------------
-#    ___  ___  _ ____
-#   / _ \/ _ \(_) __/__  __ __
-#  / , _/ ___/ /\ \/ _ \/ // /
-# /_/|_/_/  /_/___/ .__/\_, /
-#                /_/   /___/
-#
-#           servo1.py
-#  Basic example script to control a servo
-#  connected to GPIO17 using the Gpiozero library.
-#
-# Use CTRL-C to break out of While loop.
-#
-# Author : Matt Hawkins
-# Date   : 01/01/2018
-#
-# https://www.raspberrypi-spy.co.uk/tag/servo/
-#
-#--------------------------------------
 from gpiozero import Servo
 from time import sleep
 
