dced7c27e2c31bff2365d4d6bc8548b6841f2fb8
Nelson <nelson@raleygroup.com>
2019-04-05 18:33:51 -0400

    Actually added image script this time lol

---
+++ image_script.py
@@ -0,0 +1,9 @@
+import picamera
+import datetime
+
+time=datetime.datetime.now()
+current=str(time.day)+"-"+str(time.month)+"-"+str(time.year)+"_"+str(time.hour)+":"+str(time.minute)
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280,720)
+    camera.capture("Desktop/cookie/current.jpg")
