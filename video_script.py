03b28ab592bcc2404138d14af26101b371b106d8
nraley <nelson@raleygroup.com>
2019-04-15 22:47:42 -0400

    Same as last commit.

--- video_script.py
+++ video_script.py
@@ -1,15 +1,15 @@
 import os
+from datetime import datetime, timedelta
+import threading
 import time
 
-avcmd = "avconv -t 10 -i /dev/video0 "
-
-
+avcmd = "avconv -t 60 -i /dev/video0 "
 mission_complete = datetime.now() + timedelta(hours=2)
 
 def vidcap():
     now = datetime.now()
     if now < mission_complete:
-        threading.Timer(10.0, vidcap).start()
+        threading.Timer(61.0, vidcap).start()
         vidstr = "~/CS21proj/Videos/" + now.strftime("%m-%d.%H.%M.%S.avi")
 
         os.system(avcmd + vidstr)
