c45412e071afb624406fe741631b011c6449c383
Nelson Raley <nelson@raleygroup.com>
2019-05-06 15:02:51 -0400

    First push from final SBC

--- gimbal.py
+++ gimbal.py
@@ -6,20 +6,20 @@ from time import sleep
 #Pitch - Datatype is float. Measurements in Degrees of Angle. (-90 to 90 Degrees)
 #Roll - Datatype is float. Measurements in Degrees of Angle. (-180 to 180 Degrees)
 
-xGPIO=4
-yGPIO=17
+xGPIO=19
+yGPIO=26
 
 Yaw = 0
 Pitch = 45
 Roll = 0
 
-print("Using GPIO4 for xServo")
-print("Using GPIO5 for yServo")
+print("Using GPIO19 for xServo")
+print("Using GPIO26 for yServo")
 print("Using Gpiozero defaults for the servo class")
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(04, GPIO.OUT)
-pwm=GPIO.PWM(04,50)
+GPIO.setup(19, GPIO.OUT)
+pwm=GPIO.PWM(19,50)
 pwm.start(0)
 
 def SetAngle(angle):
@@ -28,10 +28,10 @@ def SetAngle(angle):
     #(10.75-2)/180 = 0.048611
     #With rotor facing down, 0 degrees is full counterclockwise, 180 is full clockwise
     duty = angle * 0.048611 + 2
-    GPIO.output(04, True)
+    GPIO.output(19, True)
     pwm.ChangeDutyCycle(duty)
     sleep(1)
-    GPIO.output(04, False)
+    GPIO.output(19, False)
     pwm.ChangeDutyCycle(0)
 
 # def FindTilt(pitch):
